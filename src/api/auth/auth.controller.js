2584bb596b576bf46a175fd01ccaf99adf0f557a
Unknown <alex.florin235@yahoo.com>
2017-08-22 21:53:46 +0300

    Auth controller also uses constants.

--- src/api/auth/auth.controller.js
+++ src/api/auth/auth.controller.js
@@ -11,13 +11,13 @@ controller.authenticate = (req, res) => {
     }, (err, user) => {
         if (err) throw err;
         if (!user || !user.comparePassword(req.body.password)) {
-            res.status(401).json({ message: 'Authentication failed' });
+            res.status(constants.HTTP_CODES.UNAUTHORIZED).json({ message: 'Authentication failed' });
         } else if (user) {
             let token = jwt.sign({ email: user.email }, config.secret, {
-                expiresIn: 1440 * 60 // expires in 24 hours
+                expiresIn: constants.TOKEN_EXPIRE_TIME,
             });
 
-            res.status(200).json({
+            res.status(constants.HTTP_CODES.OK).json({
                 success: true,
                 message: 'Enjoy your token!',
                 token: token
