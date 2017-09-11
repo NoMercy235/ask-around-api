47f9fd33f0f165120757357de02114f3daec3c5e
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2017-09-11 21:25:02 +0300

    Added moment.js to the project and used it to generate the question's created_at field.

--- src/models/question.js
+++ src/models/question.js
@@ -1,3 +1,5 @@
+let moment = require('moment');
+
 const MODEL = 'Question';
 
 let mongoose = require('mongoose');
@@ -5,6 +7,12 @@ let schema = new mongoose.Schema({
     _creator: { type: String, ref: 'User' },
     title: { type: String, required: true },
     content: { type: String, required: true },
+    created_at: { type: Date },
+});
+
+schema.pre('save', function (next) {
+    if (!this.created_at) this.created_at = moment().format();
+    next();
 });
 
 module.exports = mongoose.model(MODEL, schema);
