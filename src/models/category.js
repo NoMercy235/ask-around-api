b784c0c0f155371d5ac742b0709fef2a11288897
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2017-09-12 21:47:59 +0300

    Added validation for the category resource.

--- src/models/category.js
+++ src/models/category.js
@@ -10,6 +10,14 @@ let schema = new mongoose.Schema({
     questions: [{ type: mongoose.Schema.Types.ObjectId, ref: QUESTION }],
 });
 
+schema.path('name').validate(function (value) {
+    return value && value.length >= 10 && value.length <= 100;
+}, 'Name must be have at between 10 and 100 characters.');
+
+schema.path('description').validate(function (value) {
+    return value && value.length <= 2000;
+}, 'Name must be have less then 2000 characters.');
+
 module.exports = {
     model: mongoose.model(MODEL, schema),
     key: MODEL
